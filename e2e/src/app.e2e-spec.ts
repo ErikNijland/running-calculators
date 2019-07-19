fbf491b6278e3631ab9320e35c46744887ca1b0a
Erik Nijland <erik.nijland.osv@fedex.com>
2019-07-19 15:20:17 +0200

    move timetable to separate page

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to pace-calculator!');
+    expect(page.getTitleText()).toEqual('Welcome to running-calculators!');
   });
 
   afterEach(async () => {
--- src/modules/app/app.routes.ts
+++ src/modules/app/app.routes.ts
@@ -2,27 +2,35 @@ import {Routes} from '@angular/router';
 import {PaceCalculatorComponent} from '../calculator/components/calculators/pace-calculator/pace-calculator';
 import {DistanceCalculatorComponent} from '../calculator/components/calculators/distance-calculator/distance-calculator';
 import {TimeCalculatorComponent} from '../calculator/components/calculators/time-calculator/time-calculator';
+import {TimetableComponent} from "../timetable/components/timetable/timetable.component";
 
 export const appRoutes: Routes = [
   {
     path: 'distance',
     component: DistanceCalculatorComponent,
     data: {
-      title: 'Distance calculator: pace + time = distance'
+      title: 'Distance calculator'
     }
   },
   {
     path: 'pace',
     component: PaceCalculatorComponent,
     data: {
-      title: 'Pace calculator: distance + time = pace'
+      title: 'Pace calculator'
     }
   },
   {
     path: 'time',
     component: TimeCalculatorComponent,
     data: {
-      title: 'Time calculator: distance + pace = time'
+      title: 'Time calculator'
+    }
+  },
+  {
+    path: 'timetable',
+    component: TimetableComponent,
+    data: {
+      title: 'Timetable'
     }
   },
   {
